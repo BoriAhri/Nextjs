9bcc7faebe2f582f146fba9e2e3c082708290a26
정현우 <seraphimhw@gmail.com>
2024-09-11 20:00:09 +0900

    시간도 저장

--- pages/api/write.js
+++ pages/api/write.js
@@ -1,6 +1,13 @@
-export default function handler(req, res) {
+import { connectDB } from "@/util/database"
+
+export default async function handler(req, res) {
     if (req.method == 'POST'){
+        console.log(req.body)
         let time = new Date().toLocaleString('ko-KR', { timeZone: 'Asia/Seoul' })
-        res.status(200).json(time)
+        let db = (await connectDB).db('forum2')
+        let result = db.collection('post').insertOne({...req.body, currentTime: time})
+    
+        res.redirect(302, '/')
     }
 }
+
